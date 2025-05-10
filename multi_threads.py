fbfdf6e0b9b71a8392b0edc5fdc3f365d4491142
M M Nair <emem.nair@gmail.com>
2025-05-10 11:31:53 +0530

    Changed the author information and added a new comment

--- multi_threads.py
+++ multi_threads.py
@@ -1,4 +1,4 @@
-#created by mushrifa
+#created by mm nair- the trainer
 import threading
 import time
 
@@ -15,3 +15,5 @@ try:
         th1.start()
 except:
     print("Unable to start thread.")
+
+# This is a sample multi threads example
